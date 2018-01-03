87779ff4798ce2825892913e77097521fec31060
raulvillares <rvillares@alea-soluciones.com>
2018-01-03 15:18:18 +0100

    [RED] Projects found but without requirements

--- spec.py
+++ spec.py
@@ -3,17 +3,32 @@ from doublex import Stub, when
 from expects import expect, equal
 
 from projects_finder import ProjectsFinder
-from outdated_packages_reporter import OutdatedPackagesReporter, NO_PROJECTS_FOUND_MESSAGE
+from requirements_finder import RequirementsFinder
+from outdated_packages_reporter import OutdatedPackagesReporter, NO_PROJECTS_FOUND_MESSAGE, NO_REQUIREMENTS_MESSAGE
 
 EMPTY_PROJECTS_LIST = []
+A_PROJECT_LIST = ['first_project_name']
 
 with description('Outdated packages reporter'):
     with context('when no projects found'):
-        with it('reports no projects found'):
+        with it('reports no projects found message'):
             projects_finder = Stub(ProjectsFinder)
             when(projects_finder).find_all().returns(EMPTY_PROJECTS_LIST)
-            reporter = OutdatedPackagesReporter(projects_finder)
+            requirements_finder = RequirementsFinder(projects_finder)
+            reporter = OutdatedPackagesReporter(requirements_finder)
 
             report = reporter.generate_report()
 
             expect(report).to(equal(NO_PROJECTS_FOUND_MESSAGE))
+
+    with context('when projects found'):
+        with context('without requirements'):
+            with it('reports no requirements found message'):
+                projects_finder = Stub(ProjectsFinder)
+                when(projects_finder).find_all().returns(A_PROJECT_LIST)
+                requirements_finder = RequirementsFinder(projects_finder) 
+                reporter = OutdatedPackagesReporter(requirements_finder)
+
+                report = reporter.generate_report()
+
+                expect(report).to(equal(NO_REQUIREMENTS_MESSAGE))

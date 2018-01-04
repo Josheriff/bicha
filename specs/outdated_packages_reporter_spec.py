22d7108a3c68c3134ef7f787bc8acb0146e7a55d
raulvillares <rvillares@alea-soluciones.com>
2018-01-04 09:31:51 +0100

    [REFACTOR] Created constants for the sake of readability

--- specs/outdated_packages_reporter_spec.py
+++ specs/outdated_packages_reporter_spec.py
@@ -3,7 +3,6 @@ from doublex import Stub, when
 from expects import expect, equal
 
 from requirements_finder import RequirementsFinder
-from project_requirements import ProjectRequirements
 from outdated_packages_reporter import (OutdatedPackagesReporter,
                                         NO_PROJECTS_FOUND_MESSAGE,
                                         NO_REQUIREMENTS_MESSAGE,
@@ -12,6 +11,8 @@ from outdated_packages_reporter import (OutdatedPackagesReporter,
 EMPTY_PROJECTS_LIST = []
 A_LIST_OF_PROJECTS_WITHOUT_REQUIREMENTS = ['project_without_requirements_1']
 A_LIST_OF_PROJECTS_WITH_UP_TO_DATE_REQUIREMENTS = ['project_with_up_to_date_requirements_1']
+PROJECT_WITHOUT_REQUIREMENTS = {'project_name':'project_without_requirements_name', 'requirements':[]}
+PROJECT_WITH_REQUIREMENTS = {'project_name':'project_with_requirements_name', 'requirements':['a_requirement']}
 
 with description('Outdated packages reporter') as self:
     with before.each:
@@ -29,7 +30,7 @@ with description('Outdated packages reporter') as self:
     with context('when projects found'):
         with context('without requirements'):
             with it('reports no requirements found message'):
-                when(self.requirements_finder).find_all().returns([{'project_name':'fulanito', 'requirements':[]}])
+                when(self.requirements_finder).find_all().returns([PROJECT_WITHOUT_REQUIREMENTS])
 
                 report = self.reporter.generate_report()
 
@@ -39,7 +40,7 @@ with description('Outdated packages reporter') as self:
             with context('all requirements up to date'):
                 with it('reports all requirements up to date message'):
                     requirements_finder = Stub(RequirementsFinder)
-                    when(requirements_finder).find_all().returns([{'project_name':'fulanito', 'requirements':['hola']}])
+                    when(requirements_finder).find_all().returns([PROJECT_WITH_REQUIREMENTS])
                     reporter = OutdatedPackagesReporter(requirements_finder)
                     report = reporter.generate_report()
                     

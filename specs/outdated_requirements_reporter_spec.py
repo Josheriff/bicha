6a41107ad443ead0da360f773a91ab149f5905be
raulvillares <rvillares@alea-soluciones.com>
2018-01-05 14:06:26 +0100

    [GREEN] Added test with all possible cases at the same time

--- specs/outdated_requirements_reporter_spec.py
+++ specs/outdated_requirements_reporter_spec.py
@@ -64,3 +64,23 @@ with description('Outdated requirements reporter') as self:
                 expected_report = Report()
                 expected_report.add_project_report(project_report)
                 expect(report).to(equal(expected_report))
+
+        with context('and project with all requirements up to date'):
+            with context('and project with all requirements outdated'):
+                with context('and project with some requirements up to date and some outdated'):
+                    with it('reports only projects with their outdated requirements'):
+                        when(self.projects_finder).find_all().returns([PROJECT_WITH_UP_TO_DATE_REQUIREMENTS,
+                                                                       PROJECT_WITH_OUTDATED_REQUIREMENTS,
+                                                                       PROJECT_WITH_UP_TO_DATE_AND_OUTDATED_REQUIREMENTS
+                                                                       ])
+
+                        report = self.reporter.generate_report()
+
+                        expected_report = Report()
+                        project_report_1 = ProjectReport(project_name=PROJECT_WITH_OUTDATED_REQUIREMENTS.project_name,
+                                                       outdated_requirements=[OUTDATED_REQUIREMENT])
+                        project_report_2 = ProjectReport(project_name=PROJECT_WITH_UP_TO_DATE_AND_OUTDATED_REQUIREMENTS.project_name,
+                                                        outdated_requirements=[OUTDATED_REQUIREMENT])
+                        expected_report.add_project_report(project_report_1)
+                        expected_report.add_project_report(project_report_2)
+                        expect(report).to(equal(expected_report))
